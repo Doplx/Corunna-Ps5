06bf54a74dd0e79b711469fa95e0eb817b2e89f2
Ak47Twex <124576368+Doplx@users.noreply.github.com>
2026-04-19 13:47:25 +0300

    Update platform_module.js

--- platform_module.js
+++ platform_module.js
@@ -1,34 +1,11 @@
-let m_14669ca3b1519ba2a8f40be287f646d4d7593eb0 = () => {
-    let platformState = {
-        versionFlags: {
-            POINTER_MASK: 0xFFFFFFFFFF,
-            JIT_BASE_OFFSET: 0x123456,
-            HEAP_LIMIT: 0x7FFFFFFFF,
-        },
-        firmwareVersion: "10.60",
-    };
+console.log("[PLATFORM] Checking PS5 compatibility");
 
+let platformChecker = (() => {
     return {
-        platformState,
-
-        stripPointerTag: (ptr) => {
-            return ptr & platformState.versionFlags.POINTER_MASK;
+        isSupported() {
+            const platform = navigator.platform;
+            const userAgent = navigator.userAgent;
+            return platform.includes("PlayStation") && userAgent.includes("10.60");
         },
-
-        init: (telemetry, baseUrl, cookies, platform, userAgent) => {
-            if (platform.includes("PlayStation") && userAgent.includes("10.60")) {
-                platformState.versionFlags["JIT_ENABLED"] = true;
-                platformState.versionFlags["sandboxed"] = true;
-                return true;
-            } else {
-                return false;
-            }
-        },
-
-        alignMemoryAddress: (address) => {
-            return address & ~0x7;
-        },
-
-        getOffsets: () => platformState.versionFlags,
     };
-};
+})();
