0626a506d96ec14955391e59473779b291ac2e55
Ak47Twex <124576368+Doplx@users.noreply.github.com>
2026-04-19 19:34:05 +0300

    Refactor Hello World payload display logic

--- payloads/hello_world.js
+++ payloads/hello_world.js
@@ -1,17 +1,15 @@
-console.log("[PAYLOAD] Hello World Payload Executing...");
+console.log("[PAYLOAD] Executing Hello World payload...");
 
-(function helloWorld() {
-    const message = document.createElement('div');
-    message.style.position = 'absolute';
-    message.style.top = '50%';
-    message.style.left = '50%';
-    message.style.transform = 'translate(-50%, -50%)';
-    message.style.background = 'black';
-    message.style.color = 'white';
-    message.style.padding = '20px';
-    message.style.zIndex = '9999';
-    message.style.border = '2px solid white';
-    message.innerText = "HELLO WORLD!";
-
-    document.body.appendChild(message);
+(function () {
+    const div = document.createElement("div");
+    div.style.position = "fixed";
+    div.style.top = "50%";
+    div.style.left = "50%";
+    div.style.transform = "translate(-50%, -50%)";
+    div.style.backgroundColor = "black";
+    div.style.color = "white";
+    div.style.padding = "20px";
+    div.style.zIndex = "1000";
+    div.innerText = "HELLO WORLD!";
+    document.body.appendChild(div);
 })();
